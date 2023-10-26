04bce15350b41c611d6091776c95e6ab74535f92
likemoses2025 <mose233@gmail.com>
2023-10-26 16:43:34 +0900

    First Commit

---
+++ frontend/App.tsx
@@ -0,0 +1,14 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+      <MaterialCommunityIcons name="home" color={'#4F8EF7'} size={30} />
+    </View>
+  );
+};
+
+export default App;
---
+++ frontend/src/assets/styles/globalStyle.js
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+const globalStyles = StyleSheet.create({
+  backgroundWhite: {
+    backgroundColor: '#ffffff',
+  },
+  flex: {
+    flex: 1,
+  },
+  opacity40: {
+    backgroundColor: '#000000',
+    opacity: 0.4,
+  },
+});
+
+export default globalStyles;
