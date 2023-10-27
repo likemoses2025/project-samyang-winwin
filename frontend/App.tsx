fc68ceec1067d7a306301cf3f5e867e0dcc648d8
likemoses2025 <mose233@gmail.com>
2023-10-27 13:22:49 +0900

    Teest

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,14 +1,26 @@
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, SafeAreaView} from 'react-native';
 import React from 'react';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import {scaleFontSize} from './src/assets/styles/scaling';
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
+    <SafeAreaView>
+      <Text style={styles.font1}>
+        고구마 개나리 오나리 독다리 팔다리 캥다리 오공기2
+      </Text>
+      <Text style={{fontSize: 30}}>
+        고구마 개나리 오나리 독다리 팔다리 캥다리 오공기
+      </Text>
       <MaterialCommunityIcons name="home" color={'#4F8EF7'} size={30} />
-    </View>
+    </SafeAreaView>
   );
 };
+const styles = StyleSheet.create({
+  font1: {
+    fontFamily: 'NotoSansKR-Bold',
+    fontSize: scaleFontSize(30),
+  },
+});
 
 export default App;
